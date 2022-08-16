e87d2fcd2ac3836d83b1b22cb5d70b6e67d2d6ea
Sweekriti Satpathy <swsat@microsoft.com>
2022-08-16 16:32:57 -0700

    tryhing things

---
+++ repro_lifecycle_events/ReproLifecycleEventsCrash/ReproLifecycleEventsCrash/MyWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ReproLifecycleEventsCrash
+{
+
+    public class MyWindow : Window
+    {
+        public MyWindow() : base()
+        {
+            Activated += MyWindow_Activated;
+        }
+
+        private void MyWindow_Activated(object sender, EventArgs e)
+        {
+            Debug.WriteLine("SWEEKY ACTIVATED");
+        }
+
+        public MyWindow(Page page) : base(page)
+        {
+        }
+    }
+}
+
